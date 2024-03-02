1604b3e446d0a01e3c47f87c0d4950dadfbcd487
Hannan Butt <hannanbutt1256@gmail.com>
2024-03-02 22:54:50 +0500

    commit

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return(
+<div>
+    <h2>Page Not Found </h2>
+  <div>Error 404</div>
+  <Link href="/">Return Home</Link>
+</div>
+)};
+
+export default NotFound;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Links from "./links/Link";
+
+const Navbar = () => {
+  return(
+<div>
+    <div>Logo</div>
+    <Links />
+</div>
+  )
+};
+
+export default Navbar;
