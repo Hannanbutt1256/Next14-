1eef688afd35099e9078a0864a8859100bcf735e
Hannan Butt <hannanbutt1256@gmail.com>
2024-03-07 03:09:01 +0500

    commit

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import styles from "./links.module.css"
+import NavLink from "./navLink/navLink"
+const Links = () => {
+    const links =[
+        {
+            title : "Home",
+            path  : "/",
+ 
+        },
+        {
+            title : "About",
+            path  : "/about",
+
+        },
+        {
+            title : "Contact",
+            path  : "/contact",
+
+        },
+        {
+            title : "Blog",
+            path  : "/blog",
+
+        },
+    ]
+// Temporary
+const session = true;
+const isAdmin = true;
+
+
+  return(
+  <div className={styles.links}>
+    {
+        links.map(link=>(
+        <NavLink item={link} key={link.title}/>
+        ))}
+        {
+            session ? (
+                <>
+                { isAdmin && <NavLink item={{title:"Admin",path:"/admin"}}/>}
+                <button className={styles.logout}>Logout</button>
+                </>
+            ) : (
+                <NavLink item ={{title:"Login" , path:"/admin"}}/>
+            )
+        }
+  </div>
+  )
+};
+
+export default Links;
