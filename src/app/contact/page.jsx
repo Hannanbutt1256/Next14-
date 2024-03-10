1a27a75fa502e2ad916470e2a44fa0f4f3307e75
Hannan Butt <hannanbutt1256@gmail.com>
2024-03-10 23:12:05 +0500

    commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,18 @@
+// "use client";
 import Image from "next/image";
 import styles from "./contact.module.css";
+// import { useEffect, useState } from "react";
 const ContactPage = () => {
+	// * For Resolving Hydration error we can also use the method of importing dynamically
+	// * e.g: const HydrationNoSSR = dynamic(()=>"@components/Hydration",{ssr:false})
+	// * we can also do it using suppressHydrationWarning in html Tag........
+	// * Understanding the Hydration Error in CSR and SSR.....
+	// const [Random, setRandom] = useState(false);
+	// useEffect(() => {
+	// 	setRandom(true);
+	// }, []);
+	// const a = Math.random();
+	// console.log(a);
 	return (
 		<div className={styles.container}>
 			<div className={styles.imageContainer}>
@@ -8,6 +20,8 @@ const ContactPage = () => {
 			</div>
 			<div className={styles.formContainer}>
 				<form action="" className={styles.form}>
+					{/*FIXME: From Above 
+					{Random && a} */}
 					<input type="text" placeholder="Name and Surname" />
 					<input type="text" placeholder="Email" />
 					<input type="text" placeholder="Phone Number (Optional)" />
