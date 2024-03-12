aa487b7cdc694a00b998edaaf305e51ff53f0472
Hannan Butt <hannanbutt1256@gmail.com>
2024-03-13 00:30:58 +0500

    commit

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
-const SinglePostPage = () => {
+const getData = async (slug) => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+	if (!res.ok) {
+		throw new Error("Something went wrong");
+	}
+	return res.json();
+};
+const SinglePostPage = async ({ params }) => {
+	const { slug } = params;
+	const post = await getData(slug);
 	return (
 		<div className={styles.container}>
 			<div className={styles.imgContainer}>
@@ -12,7 +21,7 @@ const SinglePostPage = () => {
 				/>
 			</div>
 			<div className={styles.textContainer}>
-				<h1 className={styles.title}>Title</h1>
+				<h1 className={styles.title}>{post.title}</h1>
 				<div className={styles.detail}>
 					<Image
 						className={styles.avatar}
@@ -30,12 +39,7 @@ const SinglePostPage = () => {
 						<span className={styles.detailValue}>01.01.2024</span>
 					</div>
 				</div>
-				<div className={styles.content}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-					blanditiis numquam non quos neque, dolorum molestias quo nam nobis
-					labore nemo cum laudantium unde ipsam eum, eveniet eius at
-					perspiciatis?
-				</div>
+				<div className={styles.content}>{post.body}</div>
 			</div>
 		</div>
 	);
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,20 +1,24 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
-const BlogPage = () => {
+const getData = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+		cache: "no-store",
+		// ? We can also use "{next:{revalidate :3600 }}" to refresh our data accordint to our need....
+	});
+	if (!res.ok) {
+		throw new Error("Something went wrong");
+	}
+	return res.json();
+};
+const BlogPage = async () => {
+	const posts = await getData();
 	return (
 		<div className={styles.container}>
-			<div className={styles.post}>
-				<PostCard />
-			</div>
-			<div className={styles.post}>
-				<PostCard />
-			</div>
-			<div className={styles.post}>
-				<PostCard />
-			</div>
-			<div className={styles.post}>
-				<PostCard />
-			</div>
+			{posts.map((post) => (
+				<div className={styles.post} key={post.id}>
+					<PostCard post={post} />
+				</div>
+			))}
 		</div>
 	);
 };
